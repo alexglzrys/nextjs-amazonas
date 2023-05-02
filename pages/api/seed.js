4d39e4fa7c3bb8b75b85c4adc2f23b20fd22a403
Alejandro González Reyes <alexglzrys@gmail.com>
2023-05-02 17:36:40 -0600

    seed productos

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,12 +1,15 @@
 import db from '@/utils/db'
 import User from '@/models/User'
 import data from '@/utils/data';
+import Product from '@/models/Product';
 
 const handler = async(req, res) => {
     // Sembrar usuarios dummy en la colección users 
     await db.connect();
     await User.deleteMany()
     await User.insertMany(data.users)
+    await Product.deleteMany()
+    await Product.insertMany(data.products)
     await db.disconnect()
     res.send({message: 'datos sembrados correctamente'});
 }
